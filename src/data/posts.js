c18f06c892c2dec963c1de24bd494b09095d2f41
Erwin <erwin011895@gmail.com>
2021-04-06 14:49:16 +0700

    Revert "Updates"
    This reverts commit a63e3aa3249dc245b1275b2604140beedc1f3e32.

---
+++ src/data/posts.js
@@ -0,0 +1,201 @@
+const Posts = [
+	{
+		title: "Start University Life",
+		subtitle: "Binus University - Sept 2013",
+		contents: [
+			{
+				text: "My first year in college life as Binusian 2017. This is a fullteam member of my program's. Computer Science and Statistic Program 2013.",
+				image: "images/himstat_2013.jpg",
+				imageAlt: "CS-Stat 2013 Binus University",
+			},
+			{
+				text: "Semester 2, Last Calculus class, CS-Stat, CS-Math & Wikaria Gazali.",
+				image: "images/himstat_kalkulus.jpg",
+				imageAlt: "CS-Stat 2013 Calculus Class Binus University",
+			},
+			{
+				text: "MATIC (Mathematic and Statistic Competition) event in 2013.",
+				image: "images/himstat_matic_2013.jpg",
+				imageAlt: "Event Matic 2013 at Binus University",
+			},
+		],
+	},
+	{
+		title: `Organization Experience - BSLC`,
+		subtitle: `BSLC Kemanggisan - 2014 to 2016`,
+		contents: [
+			{
+				text: "Training Vacation X-Over (read: Crossover) Quality in 2015",
+				image: "images/bslc_tvxq_2015.jpg",
+				imageAlt: "BSLC TVXQ 2015",
+			},
+			{
+				text: "This is my very first organization experience. I choose this organization because of its learning environment. With guidance from Binus's staff, I had good friends and some experiences that changed my thinking perspective and mindset.",
+				image: "images/bslc_tvxq_2017.jpg",
+				imageAlt: "BSLC TVXQ 2017",
+			},
+		],
+	},
+	{
+		title: `Buddy Coordinator - Freshmen Enrichment Program`,
+		subtitle: `Binus - 2014 to 2016`,
+		contents: [
+			{
+				text: "Every year, Binus held a Freshment Enrichment Program. Buddy Corrdinator is seniors that will help and assigned to run orientation program to freshmen. It was fun to meet many people and learn about coordination here.",
+				image: "images/binus_fep_2018.jpg",
+				imageAlt: "Buddy - BC Togetherness in 2014",
+			},
+			{
+				text: "Inauguration Binusian 2020 at JCC",
+				image: "images/binus_fep_2020.jpg",
+				imageAlt: "Inauguration Binusian 2020 at JCC",
+			},
+			{
+				text: "FEP Class in 2016",
+				image: "images/binus_fep_2020_csstat.jpg",
+				imageAlt: "FEP Class CS-STAT 2016",
+			},
+		],
+	},
+	{
+		title: `First Working Experience - Parttime Software Engineer`,
+		subtitle: `Lazato.com - Mar to Nov 2016`,
+		contents: [
+			{
+				text: "My first role is Android App Engineer, later changed to Backend Engineer. Here is my first meetup with Lazato members.",
+				image: "images/lazato_first_meetup.jpg",
+				imageAlt: "Lazato First Meetup",
+			},
+			{
+				text: "Here a photo when Lazato just open a new office.",
+				image: "images/lazato_opening_celebration.jpg",
+				imageAlt: "Lazato Office Opening",
+			},
+			{
+				text: "Sometimes the CEO suddenly take photo when we were working. This is one of his collections.",
+				image: "images/lazato_in_office.jpg",
+				imageAlt: "Inside Lazato Office",
+			},
+		],
+	},
+	{
+		title: `Binus' 3+1 Program - Internship`,
+		subtitle: `MNC Kapital - Mar 2017 to Aug 2017`,
+		contents: [
+			{
+				text: "This is where I work as IT Intern on 2017. I was researching PWA for a new product.",
+				image: "images/mnc_kapital_indonesia.jpg",
+				imageAlt: "MNC Kapital Indonesia",
+			},
+		],
+	},
+	{
+		title: `Kopem Ekayana`,
+		subtitle: `Vihara Ekayana Arama - Jun 2017`,
+		contents: [
+			{
+				text: "Around June 2017, I invited by my friend to join as volunteer here. I make many friends here. This is a photo of all volunteer on Ekayana's Chinese New Year.",
+				image: "images/ekayana_kopem_imlek.jpg",
+				imageAlt: "Ekayana Chinese New Year 2019",
+			},
+			{
+				text: `Later I joined the Kopem Organization as a staff of HRD, developing Kopem members in "self development" context. This is one of our Leadership Camp when I was still a participant on 2019.`,
+				image: "images/ekayana_kopem_blc_2019.jpg",
+				imageAlt: "Bodhisatva Leadership Camp 2019",
+			},
+		],
+	},
+	{
+		title: `Achievement! Top 50 in <a href="https://www.kalibrr.com/advice/2017/11/kode-indonesia-2017-kompetisi-code-career-terbesar-tahun-ini">Kode Indonesia Challange</a>`,
+		subtitle: `Programming Contest by <a href="https://www.kalibrr.com/">Kalibrr Indonesia</a> - 26 Oct 2017`,
+		contents: [
+			{
+				text: `Around October, 2-3 weeks before the Grand Final, I found free Programming Contest and joined the online round. After the online round finished, I got a mail to join the Grand Final as Top 50 Finalist. On D-1, all finalist invited to dinner. I met with friends from <a href="https://jollybee.binus.ac.id/">Jollybee Binus Team</a> there. Here is a photo with "Kevin Sogo", formerly 2nd winner of Google Code Jam.`,
+				image: "images/kode_indonesia_with_kevin_sogo.jpg",
+				imageAlt: "Photo with Kevin Sogo",
+			},
+			{
+				text: "This is the Grand Final of Kode Indonesia 2017.",
+				image: "images/kode_indonesia_final_round.jpg",
+				imageAlt: "Kode Indonesia Final Round",
+			},
+			{
+				text: `This is the last scoreboard I got from the website. I think 31st place is not bad for not-so-routine competitive programmer like me. It was a great experience. Sometimes I play in HackerRank or Codeforces for fun.`,
+				image: "images/kode_indonesia_scoreboard.jpg",
+				imageAlt: "Kode Indonesia, Score Board",
+			},
+		],
+	},
+	{
+		title: `Parttime Backend Engineer`,
+		subtitle: `<a href="https://ralali.com">Ralali.com</a> - Nov 2017 to Feb 2018`,
+		contents: [
+			{
+				text: "So, I got the chance to Part Time at Ralali.com as Backend Developer. I learnt many things that is different from previous company.",
+				image: "images/ralali_office_arjuna.jpg",
+				imageAlt: "Ralali Office Arjuna",
+			},
+		],
+	},
+	{
+		title: `Become Fulltime Software Engineer`,
+		subtitle: `<a href="https://ralali.com">Ralali.com</a> - Mar 2018 to Feb 2019`,
+		contents: [
+			{
+				text: "First fulltime experience as Backend Developer. Here is photo of my birthday on January 2019.",
+				image: "images/ralali_full_time.jpg",
+				imageAlt: "My birthday at Ralali Office",
+			},
+		],
+	},
+	{
+		title: `Graduation`,
+		subtitle: `<a href="https://binus.ac.id">Binus University</a> - 12 Dec 2018`,
+		contents: [
+			{
+				text: "I got the title: S.Kom., S.Si (Sarjana Komputer & Sarjana Sains)",
+				image: "images/wisuda_binus.jpg",
+				imageAlt: "Binus Graduation",
+			},
+		],
+	},
+	{
+		title: `Life as Lead Engineer at Ralali.com`,
+		subtitle: `<a href="https://www.ralali.com">Ralali.com</a> - Mar 2019 to Jan 2020`,
+		contents: [
+			{
+				text: `First team: GRIFFIN, handle initiation of <a href="https://www.ralali.com/finance-product">Finance Product</a>.`,
+				image: "images/griffin_1st_sprint_finished.jpg",
+				imageAlt: "Ralali Griffin Team",
+			},
+			{
+				text: "Ralali moved to Bluegreen Office and had to restructuring Engineering teams. Here's the Website Performance Team.",
+				image: "images/ralali_swb_1.jpeg",
+				imageAlt: "Ralali Service Website Team",
+			},
+			{
+				text: "Sometimes, I get chances to show on sharing knowledges to the Ralalians.",
+				image: "images/ralali_fed_0.jpeg",
+				imageAlt: "Ralali Front End Discussion",
+			},
+			{
+				text: "Thank you for experiences in these 2 years.",
+				image: "images/ralali_last_day.jpg",
+				imageAlt: "Ralali Erwin's Last Day",
+			},
+		],
+	},
+	{
+		title: `Join Shopee ID as Backend Engineer`,
+		subtitle: ` <a href="https://shopee.co.id/">Shopee</a> - Feb 2020`,
+		contents: [
+			{
+				text: "Shopee ID Tech Team",
+				image: "images/shopee_3_interns_last_day.jpg",
+				imageAlt: "Shopee Tech Team",
+			},
+		],
+	},
+];
+
+export default Posts;
